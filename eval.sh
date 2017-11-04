5da610eeaf7143cad2f1affd8e14bb599024c94a
Young H. Oh <garion9013@gmail.com>
2017-11-05 03:50:56 +0900

    eval.sh. add darknet

--- eval.sh
+++ eval.sh
@@ -3,7 +3,9 @@
 # ./darknet classifier predict cfg/imagenet1k.data cfg/alexnet.cfg alexnet.weights data/dog.jpg
 # ./darknet classifier predict cfg/imagenet1k.data cfg/extraction.cfg extraction.weights data/giraffe.jpg
 # ./darknet classifier predict cfg/imagenet1k.data cfg/darknet.cfg darknet.weights data/giraffe.jpg
-./darknet classifier predict cfg/imagenet1k.data cfg/darknet.cfg darknet.weights data/giraffe.jpg
+# ./darknet classifier predict cfg/imagenet1k.data cfg/darknet.cfg darknet.weights data/giraffe.jpg
 # ./darknet classifier predict cfg/imagenet1k.data cfg/darknet19.cfg darknet19.weights data/giraffe.jpg
 # ./darknet classifier predict cfg/imagenet1k.data cfg/alexnet.cfg alexnet.weights data/dog.jpg
 # ./darknet classifier valid cfg/imagenet1k.data cfg/alexnet.cfg alexnet.weights
+
+./darknet classifier valid cfg/imagenet1k.data cfg/darknet.cfg darknet.weights -gpus 0
